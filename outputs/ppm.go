a8fa46e4fde9ff818651d7e2cd283ada1d5c35c2
Neven Cubic <neven.cubic@gmail.com>
2023-09-06 14:41:21 +0200

    PPM file ToString complete

--- outputs/ppm.go
+++ outputs/ppm.go
@@ -9,6 +9,7 @@ import (
 )
 
 var MAX_VALUE = 255
+var MAX_LINE_LENGTH = 70
 
 func getHeaderString(c t.Canvas) string {
 	return fmt.Sprintf("P3\n%d %d\n%d", c.Width, c.Height, MAX_VALUE)
@@ -33,7 +34,7 @@ func formatValue(v float64) string {
 }
 
 func getBodyString(c t.Canvas) string {
-	lines := make([]string, c.Height)
+	lines := make([]string, 0, c.Height)
 
 	for y := 0; y < c.Height; y++ {
 		values := make([]string, c.Width*3)
@@ -43,13 +44,42 @@ func getBodyString(c t.Canvas) string {
 			values[x+1] = formatValue(pixel.Green())
 			values[x+2] = formatValue(pixel.Blue())
 		}
-		line := strings.Join(values, " ")
-		lines[y] = line
+
+		newLines := appendToLines(values)
+		lines = append(lines, newLines...)
 	}
 
 	return strings.Join(lines, "\n")
 }
 
+func appendToLines(values []string) []string {
+	currentLength := -1 // initial value won't have space before it
+	buffer := make([]string, 0, MAX_LINE_LENGTH/3)
+	lines := make([]string, 0, len(values)*3/MAX_LINE_LENGTH)
+
+	SPACE_LENGTH := 1
+
+	for _, v := range values {
+
+		expectedLength := currentLength + len(v) + SPACE_LENGTH
+
+		if expectedLength > MAX_LINE_LENGTH {
+			lines = append(lines, strings.Join(buffer, " "))
+			buffer = make([]string, 0, MAX_LINE_LENGTH/3)
+			currentLength = 0
+		}
+
+		buffer = append(buffer, v)
+		currentLength += len(v) + 1
+	}
+
+	if currentLength > 0 {
+		lines = append(lines, strings.Join(buffer, " "))
+	}
+
+	return lines
+}
+
 func ToString(c t.Canvas) string {
-	return fmt.Sprintf("%s\n%s", getHeaderString(c), getBodyString(c))
+	return fmt.Sprintf("%s\n%s\n", getHeaderString(c), getBodyString(c))
 }
--- outputs/ppm_test.go
+++ outputs/ppm_test.go
@@ -37,6 +37,7 @@ func TestToStringBody(t *testing.T) {
 
 	s := ToString(c)
 	lines := strings.Split(s, "\n")
+	lines = lines[0 : len(lines)-1] //skip empty line
 
 	if len(lines) != 6 {
 		t.Fatalf("Invalid line count %d", len(lines))
@@ -52,5 +53,50 @@ func TestToStringBody(t *testing.T) {
 
 	if lines[5] != "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255" {
 		t.Fatalf("Invalid third body line %s", lines[5])
+	}
+}
+
+func TestToStringLineSplitting(t *testing.T) {
+	c := types.CreateCanvas(10, 2)
+
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 2; j++ {
+			c.WritePixel(i, j, types.CreateColor(1, 0.8, 0.6))
+		}
+	}
+
+	s := ToString(c)
+	lines := strings.Split(s, "\n")
+	lines = lines[0 : len(lines)-1] //skip empty line
+
+	if len(lines) != 7 {
+		t.Fatalf("Invalid line count %d", len(lines))
+	}
+
+	if lines[3] != "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204" {
+		t.Fatalf("Invalid first body line %s", lines[3])
+	}
+
+	if lines[4] != "153 255 204 153 255 204 153 255 204 153 255 204 153" {
+		t.Fatalf("Invalid second body line %s", lines[4])
+	}
+
+	if lines[5] != "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204" {
+		t.Fatalf("Invalid third body line %s", lines[5])
+	}
+
+	if lines[6] != "153 255 204 153 255 204 153 255 204 153 255 204 153" {
+		t.Fatalf("Invalid forth body line %s", lines[6])
+	}
+}
+
+func TestToStringFileTerminatedByNewLine(t *testing.T) {
+	c := types.CreateCanvas(5, 3)
+	s := ToString(c)
+
+	lastChar := string(s[len(s)-1])
+
+	if lastChar != "\n" {
+		t.Fatalf("Invalid last character %s", lastChar)
 	}
 }
